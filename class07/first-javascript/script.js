83a09acddf5b9df4b1273042cd688ff3c3dfa6fe
Rich <richardjparkesq@gmail.com>
2018-12-12 18:03:42 -0500

    Added project proposal

---
+++ class07/first-javascript/script.js
@@ -0,0 +1,26 @@
+let product = 10 * 10; 
+let quotient = 100/10;
+let difference = 50-17;
+let sum = 50+17;
+let first = 'Richard';
+let last = 'Park';
+
+
+console.log(product);
+console.log(quotient);
+console.log(difference);
+console.log(sum);
+console.log(first+' '+last);
+
+console.log(`Hello my name is ${first} ${last}!!`)
+
+first = 'Spider-Man'
+
+console.log(`Hello my name is ${first} ${last}!!`)
+
+let hello = document.querySelector('h1');
+
+console.log(hello);
+
+hello.style.color = 'blue';
+hello.innerHTML = 'Hallo World emoji'
